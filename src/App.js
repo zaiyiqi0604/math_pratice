828b134ed2fbf56dc5d11ba97b4d2b67d5781055
Pippen Peng <pippen.peng@woven.com.au>
2022-01-13 00:53:19 +1100

    First page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import dog from './img/dog.png';
 import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Header from './pages/Header';
+import React, { Component } from 'react'
+class App extends Component {
+    render() {
+        var RandomNumber1 = Math.floor(Math.random() * 9) + 1;
+        var RandomNumber2 = Math.floor(Math.random() * 9) + 1;
+        console.log(RandomNumber1);
+        console.log(RandomNumber2);
+        var total = RandomNumber1 * RandomNumber2;
+        //console.log(total);
+        const rows = [];
+        // for (let i = 0; i < RandomNumber1; i++) { // for (let k=0; k < RandomNumber2; k++) { // rows.push(<img key={k} calss={k} src={dog} className="item" alt="item" />);
+        // }
+        // rows.push(<div className='clear'></div>);
+        // }
+        for (let i = 0; i < total; i++) {
+            if (i % RandomNumber2 == 0) {
+                rows.push(<div key={total + i} id={total + i} className='clear'>
+                </div>);
+            } rows.push(<img key={i} id={i} src={dog} className="item" alt="item" />);
+        }
+        return (
+            <div>
+                <Header />
+                <div className="container">
+                    <div className="row">
+                        <div className="item-wrap col-12 col-md-12">
+                            <div className="item-content"> {rows} </div>
+                        </div>
+                        <div className="col-12 col-md-12">
+                            <h2> How many dogs ? </h2>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
-
 export default App;
---
+++ src/pages/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+ 
+class Header extends Component {
+  render() {
+    return (
+      <div className="header">
+        <h1>Multiplication Table Demo application created in React App</h1>
+      </div>
+    );
+  }
+}
+ 
+export default Header
